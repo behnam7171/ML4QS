58643c3eced1ca4c602e3a96cafe5bf17c630e8e
melisnv <melisnurverir@gmail.com>
2022-06-23 12:37:47 +0300

    plots for chapter 7

--- Python3Code/mnb_crowdsignals_ch8_regression.py
+++ Python3Code/mnb_crowdsignals_ch8_regression.py
@@ -16,9 +16,9 @@ from pathlib import Path
 from sklearn.model_selection import train_test_split
 
 # Set up file names and locations.
-DATA_PATH = Path('./intermediate_datafiles/')
-DATASET_FNAME = 'chapter2_result.csv'
-RESULT_FNAME =  'chapter3_result_outliers.csv'
+DATA_PATH = Path('./mnb_intermediate_datafiles/')
+DATASET_FNAME = 'mnb_chapter2_result.csv'
+RESULT_FNAME =  'mnb_chapter3_result_outliers.csv'
 
 # Next, import the data from the specified location and parse the date index.
 try:
@@ -34,8 +34,8 @@ DataViz = VisualizeDataset(__file__)
 # Of course we repeat some stuff from Chapter 3, namely to load the dataset
 
 # Read the result from the previous chapter, and make sure the index is of the type datetime.
-DATA_PATH = Path('./intermediate_datafiles/')
-DATASET_FNAME = 'chapter5_result.csv'
+DATA_PATH = Path('./mnb_intermediate_datafiles/')
+DATASET_FNAME = 'mnb_chapter5_result.csv'
 
 DataViz = VisualizeDataset(__file__)
 
@@ -51,9 +51,9 @@ dataset.index = pd.to_datetime(dataset.index)
 
 prepare = PrepareDatasetForLearning()
 
-train_X, test_X, train_y, test_y = prepare.split_single_dataset_regression_by_time(dataset, 'hr_watch_rate', '2016-02-08 18:29:56',
+train_X, test_X, train_y, test_y = prepare.split_single_dataset_regression_by_time(dataset, 'gyr_phone_x', '2022-06-08 16:20:36',
 #                                                                                   '2016-02-08 18:29:58','2016-02-08 18:29:59')
-                                                                                   '2016-02-08 19:34:07', '2016-02-08 20:07:50')
+                                                                                   '2022-06-08 16:34:26', '2022-06-08  16:46:57')
 
 print('Training set length is: ', len(train_X.index))
 print('Test set length is: ', len(test_X.index))
@@ -65,10 +65,11 @@ print('Test set length is: ', len(test_X.index))
 
 # Select subsets of the features that we will consider:
 
-basic_features = ['acc_phone_x','acc_phone_y','acc_phone_z','acc_watch_x','acc_watch_y','acc_watch_z','gyr_phone_x','gyr_phone_y','gyr_phone_z','gyr_watch_x','gyr_watch_y','gyr_watch_z',
-                  'labelOnTable','labelSitting','labelWashingHands','labelWalking','labelStanding','labelDriving','labelEating','labelRunning',
-                  'light_phone_lux','mag_phone_x','mag_phone_y','mag_phone_z','mag_watch_x','mag_watch_y','mag_watch_z','press_phone_pressure']
+basic_features = ['acc_phone_x','acc_phone_y','acc_phone_z','gyr_phone_y','gyr_phone_z','labelOnthetable','labelSitting','labelCycling',
+                  'labelWalking','labelRunning','light_phone_lux','mag_phone_x','mag_phone_y','mag_phone_z']
+
 pca_features = ['pca_1','pca_2','pca_3','pca_4','pca_5','pca_6','pca_7']
+
 time_features = [name for name in dataset.columns if ('temp_' in name and not 'hr_watch' in name)]
 freq_features = [name for name in dataset.columns if (('_freq' in name) or ('_pse' in name))]
 print('#basic features: ', len(basic_features))
@@ -81,17 +82,18 @@ features_after_chapter_3 = list(set().union(basic_features, pca_features))
 features_after_chapter_4 = list(set().union(basic_features, pca_features, time_features, freq_features))
 features_after_chapter_5 = list(set().union(basic_features, pca_features, time_features, freq_features, cluster_features))
 
-selected_features = ['temp_pattern_labelOnTable','labelOnTable', 'temp_pattern_labelOnTable(b)labelOnTable', 'cluster',
-                     'pca_1_temp_mean_ws_120','pca_2_temp_mean_ws_120','pca_2','acc_watch_y_temp_mean_ws_120','gyr_watch_y_pse',
-                     'gyr_watch_x_pse']
+selected_features = ['temp_pattern_labelOnthetable','labelOnthetable', 'temp_pattern_labelOnthetable(b)labelOnthetable', 'cluster',
+                     'acc_phone_y_freq_0.0_Hz_ws_40','mag_phone_z_max_freq','pca_2','acc_phone_x_freq_1.9_Hz_ws_40','acc_phone_y_freq_0.5_Hz_ws_40',
+                     'gyr_phone_y_freq_1.0_Hz_ws_40']
+
 possible_feature_sets = [basic_features, features_after_chapter_3, features_after_chapter_4, features_after_chapter_5, selected_features]
 feature_names = ['initial set', 'Chapter 3', 'Chapter 4', 'Chapter 5', 'Selected features']
 
 # Let us first study whether the time series is stationary and what the autocorrelations are.
 
-dftest = adfuller(dataset['hr_watch_rate'], autolag='AIC')
+dftest = adfuller(dataset['gyr_phone_x'], autolag='AIC')
 
-plt.Figure(); autocorrelation_plot(dataset['hr_watch_rate'])
+plt.Figure(); autocorrelation_plot(dataset['gyr_phone_x'])
 DataViz.save(plt)
 plt.show()
 
@@ -185,11 +187,11 @@ for i in range(0, len(possible_feature_sets)):
 DataViz.plot_performances_regression(['Reservoir', 'RNN', 'Time series'], feature_names, scores_over_all_algs)
 
 regr_train_y, regr_test_y = learner.reservoir_computing(train_X[features_after_chapter_5], train_y, test_X[features_after_chapter_5], test_y, gridsearch=False)
-DataViz.plot_numerical_prediction_versus_real(train_X.index, train_y, regr_train_y['hr_watch_rate'], test_X.index, test_y, regr_test_y['hr_watch_rate'], 'heart rate')
+DataViz.plot_numerical_prediction_versus_real(train_X.index, train_y, regr_train_y['gyr_phone_x'], test_X.index, test_y, regr_test_y['gyr_phone_x'], 'gyr phone x')
 regr_train_y, regr_test_y = learner.recurrent_neural_network(train_X[basic_features], train_y, test_X[basic_features], test_y, gridsearch=True)
-DataViz.plot_numerical_prediction_versus_real(train_X.index, train_y, regr_train_y['hr_watch_rate'], test_X.index, test_y, regr_test_y['hr_watch_rate'], 'heart rate')
+DataViz.plot_numerical_prediction_versus_real(train_X.index, train_y, regr_train_y['gyr_phone_x'], test_X.index, test_y, regr_test_y['gyr_phone_x'], 'gyr phone x')
 regr_train_y, regr_test_y = learner.time_series(train_X[basic_features], train_y, test_X[basic_features], test_y, gridsearch=True)
-DataViz.plot_numerical_prediction_versus_real(train_X.index, train_y, regr_train_y['hr_watch_rate'], test_X.index, test_y, regr_test_y['hr_watch_rate'], 'heart rate')
+DataViz.plot_numerical_prediction_versus_real(train_X.index, train_y, regr_train_y['gyr_phone_x'], test_X.index, test_y, regr_test_y['gyr_phone_x'], 'gyr phone x')
 
 # And now some example code for using the dynamical systems model with parameter tuning (note: focus on predicting accelerometer data):
 
